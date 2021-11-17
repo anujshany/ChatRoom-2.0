69684a75194bfef2ed8e020adcfa746e4b3715a2
Anuj Shany <64464309+anujshany@users.noreply.github.com>
2021-11-17 22:12:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { ChatEngine} from 'react-chat-engine'
+
+import  ChatFeed from './components/ChatFeed'
+
+import './App.css';
+
+const App  = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="537878be-55e6-479f-bfc1-a11444f4d0dc"
+            userName="Anuj Shany"
+            userSecret="anuj@2001"
+            renderChatFeed ={(chatAppProps) =><ChatFeed {... chatAppProps}/>}
+        />
+    )
+}
+
+export default App;
