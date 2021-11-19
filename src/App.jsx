e501e4fdacc14254ea14e4a3b9fbed225d2e7294
Anuj Shany <64464309+anujshany@users.noreply.github.com>
2021-11-19 20:46:41 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { ChatEngine } from "react-chat-engine";
 
+import LoginForm from "./components/LoginForm";
 import ChatFeed from "./components/ChatFeed";
 
 import "./App.css";
 
 const App = () => {
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
       projectID="537878be-55e6-479f-bfc1-a11444f4d0dc"
-      userName="Anuj Shany"
-      userSecret="anuj@2001"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
     // vid to  be continued at 51:14
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,10 +4,27 @@ import axios from 'axios'
 const LoginForm = () =>{
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
+        const authObject = { 'Project-ID':"537878be-55e6-479f-bfc1-a11444f4d0dc", 'User-Name': username, 'User-Secret':password };
+
+        try {
+            // username | password => chatengine => give messages
+            await axios.get('https://api.chatengine.io/chats',{headers:authObject}); 
+            
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+            // works-out --> logged in
+            
+        } catch (error) {
+            //error --> try with different credentials 
+            setError('Oops, incorrect credentials ')
+        }
     }
 
     return(
@@ -22,9 +39,13 @@ const LoginForm = () =>{
                             <span>Start Chatting</span>
                         </button>
                     </div>
-                    
+                    <h2 className="error" align="center">
+                        {error}
+                    </h2>
                 </form>
             </div>
         </div>
     )
 }
+
+export default LoginForm;
