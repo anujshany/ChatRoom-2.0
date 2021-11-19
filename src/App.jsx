055d12a74a8a65e323ac48a8bc03ec976e955efd
Anuj Shany <64464309+anujshany@users.noreply.github.com>
2021-11-19 20:22:14 +0530

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { ChatEngine} from 'react-chat-engine'
+import { ChatEngine } from "react-chat-engine";
 
-import  ChatFeed from './components/ChatFeed'
+import ChatFeed from "./components/ChatFeed";
 
-import './App.css';
+import "./App.css";
 
-const App  = () => {
-    return(
-        <ChatEngine
-            height="100vh"
-            projectID="537878be-55e6-479f-bfc1-a11444f4d0dc"
-            userName="Anuj Shany"
-            userSecret="anuj@2001"
-            renderChatFeed ={(chatAppProps) =><ChatFeed {... chatAppProps}/>}
-        />
-    )
-}
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="537878be-55e6-479f-bfc1-a11444f4d0dc"
+      userName="Anuj Shany"
+      userSecret="anuj@2001"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+    // vid to  be continued at 51:14
+  );
+};
 
 export default App;
